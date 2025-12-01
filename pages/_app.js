0ec0214b80827835c3b07a9cf3833d26f59c37a1
Ismail Anes <ismailanes1993@gmail.com>
2025-11-30 22:05:10 -0500

    update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Fragment>
       <Head>
+        <link rel="icon" type="image/png" href="/img/hero/avatar.png" />
+        <link rel="apple-touch-icon" href="/img/hero/avatar.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;800;900&display=swap"
           rel="stylesheet"
